20f006bd94085fb430775070063f4b965ff7f197
fechner <sven.fechner@stud.uni-hannover.de>
2025-10-27 12:35:27 +0100

    Edited: Link Manipulation

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/static");
+  eleventyConfig.addPassthroughCopy({ "src/static/docs": "docs" });
+  eleventyConfig.addPassthroughCopy({ "src/static/images": "images" });
+
 
   return {
     dir: {
